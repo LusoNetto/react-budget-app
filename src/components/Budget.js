0ad420faa4761801c2a4d76edf70b9fd8ace2048
Luso Netto <luciano.silva137@hotmail.com>
2023-04-04 00:35:18 -0300

    add editable budget value

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,23 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
+    const { dispatch } = useContext(AppContext);
     const { budget } = useContext(AppContext);
+
+    const handleChangeBudget = (valor) => {
+        dispatch({
+            type: "SET_BUDGET",
+            action: valor,
+        }) 
+    }
+
     return (
         <div className='alert alert-secondary'>
             <span>Budget: £</span>
             <input
                 type="number"
                 value = {budget}
+                step = {10}
                 onChange= {(event) => handleChangeBudget(event.target.value)}
             ></input>
         </div>
